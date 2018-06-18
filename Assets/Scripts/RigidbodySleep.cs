7f10feeca63c786a077f5af741adbdd6785fa487
Lydia Ha <lyh12@scarletmail.rutgers.edu>
2018-06-18 11:45:28 -0400

    Creating movement of clouds & building the castle (created wall and slab)

---
+++ Assets/Scripts/RigidbodySleep.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RigidbodySleep rb = GetComponent<RigidbodySleep> ();
+		if (rb != null) rb.Sleep ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
