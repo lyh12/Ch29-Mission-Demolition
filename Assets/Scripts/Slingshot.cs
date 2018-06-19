ff0efc55d6610b1237e412396b2d47223481c871
Lydia Ha <lyh12@scarletmail.rutgers.edu>
2018-06-19 18:23:07 -0400

    Created a projectile line that follows the projectile object

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+	static private Slingshot S;
 	[Header("Set in Inspector")]
 	public GameObject prefabProjectile;
 	public float velocityMult = 8f;
@@ -16,8 +17,16 @@ public class Slingshot : MonoBehaviour {
 
 	private Rigidbody projectileRigidbody;
 
+	static public Vector3 LAUNCH_POS {
+		get {
+			if (S == null)
+				return Vector3.zero;
+			return S.launchPos;
+		}
+	}
 
 			void Awake() {
+		S = this;
 			Transform launchPointTrans = transform.Find ("LaunchPoint");
 				launchPoint = launchPointTrans.gameObject;
 				launchPoint.SetActive(false);
